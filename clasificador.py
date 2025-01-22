31cef44f1f4cf89c400764ffc07ddb1ff55be9ec
dannasalazar11 <93801258+dannasalazar11@users.noreply.github.com>
2025-01-22 16:41:35 -0500

    Update clasificador.py

--- clasificador.py
+++ clasificador.py
@@ -28,9 +28,7 @@ def preprocess_image(image):
     image = image.convert('L')  # Convertir a escala de grises
     image = image.resize((28, 28))  # Redimensionar a 28x28
     image_array = img_to_array(image) / 255.0  # Normalizar los píxeles
-    st.write(f"Dimensiones de la imagen procesada: {image_array.shape}")
-    image_array = image_array.reshape((1, 28, 28))  # Añadir dimensión batch
-    st.write(f"Dimensiones de la imagen procesada: {image_array.shape}")
+    image_array = np.expand_dims(image_array, axis=0)  # Añadir dimensión batch
     return image_array
 
 def main():
